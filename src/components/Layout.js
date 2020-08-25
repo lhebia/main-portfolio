2fb3af5c6b67d1521f03a3fb8280d9f8796d16e9
lhebia <lhebia@gmail.com>
2020-08-25 02:21:43 -0400

    Styled layout with wrapper 🤽🏽‍♀️

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const Wrapper = styled.div`
+    width: 80%;
+    max-width: 1200px;
+    margin: 0 auto;
+`;
+
 const Layout = ({ children }) => {
     return (
-        <div>
+        <Wrapper>
             <div>
                 <Header />
                 {children}
             </div>
             <Footer />
-        </div>
+        </Wrapper>
     )
 }
 
