5b3141cddbcba8da6a0b2d8aafb969f037a951ad
lhebia <lhebia@gmail.com>
2020-09-05 23:10:32 -0400

    Added skills and mapping function 🧗🏻

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-import { Wrapper, PageSection, SectionHeader, siteVars } from '../styles/globalStyles';
+import { Wrapper, PageSection, SectionHeader } from '../styles/globalStyles';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const SkillsUl = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+`;
 
 export default function Skills() {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    skills {
+                        icon
+                        skillName
+                    }
+                }
+            }
+        }
+    `);
+
+    const skills = data.site.siteMetadata.skills;
+
     return (
         <Wrapper>
             <PageSection>
                 <SectionHeader>Skills</SectionHeader>
-                <p>Something</p>
-                <p>Something</p>
+                <SkillsUl>
+                    {
+                        skills.map((skill) => {
+                            return (
+                                <li>
+                                    <p>{skill.skillName}</p>
+                                    { skill.icon }
+                                </li>
+                            )
+                        })
+                    }
+                </SkillsUl>
             </PageSection>
         </Wrapper>
     )
