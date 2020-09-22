f12f13ecb18f3aa8596730b126a3aaa83696dcdc
lhebia <lhebia@gmail.com>
2020-09-22 00:22:29 -0400

    Allowed rendering of images on blogtemplate 💡📸💥

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/Layout';
+import { Wrapper } from '../styles/globalStyles';
 
 export const query = graphql`
   query ($slug: String!) {
@@ -17,21 +18,23 @@ export const query = graphql`
 `;
 
 export default function blogTemplate(props) {
-    // const options = {
-    //     renderNode: {
-    //         "embedded-asset-block": (node) => {
-    //             const alt = node.data.target.fields.title['en-US'];
-    //             const url = node.data.target.fields.file['en-US'].url;
-    //             return <img alt={alt} src={url} />
-    //         }
-    //     }
-    // }
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title["en-US"];
+        const url = node.data.target.fields.file["en-US"].url;
+        return <img alt={alt} src={url}/>;
+      }
+    }
+  }
 
-    return (
-        <Layout>
-            <h1>{props.data.contentfulBlogPost.title}</h1>
-            <p>{props.data.contentfulBlogPost.publishedDate}</p>
-            { documentToReactComponents(props.data.contentfulBlogPost.body.json)}
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>{props.data.contentfulBlogPost.title}</h1>
+        <p>{props.data.contentfulBlogPost.publishedDate}</p>
+        {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+      </Wrapper>
+    </Layout>
+  )
 }
