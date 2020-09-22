f50fbe64c44d11f29c0b2bfbaca527a2388e903d
lhebia <lhebia@gmail.com>
2020-09-22 02:50:15 -0400

    Styled splash with placeholder image ☕️ ☕️ 👌🏻

--- src/components/HeroSplash.js
+++ src/components/HeroSplash.js
@@ -3,10 +3,14 @@ import styled from '@emotion/styled';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import { siteVars } from '../styles/globalStyles';
+import elnido from '../assets/elnido.jpg';
 
 const SplashSection = styled.section`
     min-height: 60vh;
     background-color: ${siteVars.offWhite};
+    background: url(${elnido});
+    background-position: center;
+    background-size: cover;
     display: grid;
     place-items: center;
 `;
