c3131f8a5bbbfafd11fe933b8f151ee3234f6057
lhebia <lhebia@gmail.com>
2020-11-25 23:27:12 -0500

    Re-organize layout and socialnav components 🚀

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const Layout = ({ children, headerPosition, colorA, colorB }) => {
     return (
