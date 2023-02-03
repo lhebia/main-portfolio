9d7c97b149ee4fcec34baef2a2c3d1d524df0906
lhebia <lhebia@gmail.com>
2023-02-03 18:57:48 -0500

    portfoliov3 changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import About from '../components/About';
-import Head from '../components/Head';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-import ContactForm from '../components/ContactForm';
-import HeroSplash from '../components/HeroSplash';
-import lightskyblue from '../assets/lightskyblue.jpg';
+import PortfolioV3 from '../components/PortfolioV3';
 
 export default function Home(){
   return (
-    <Layout>
-      <Head title="Portfolio" />
-      <HeroSplash background={lightskyblue} />
-      <About />
-      <Projects />
-      <Skills />
-      <ContactForm />
-    </Layout>
+    <PortfolioV3 />
   )
 }
